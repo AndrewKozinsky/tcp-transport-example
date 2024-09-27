78e9780c6d034c84ad75f85affe878a1cfaa5bf8
Andrew Kozinsky <andkozinskiy@yandex.ru>
2024-09-27 10:35:05 +0500

    Add Readme description

--- apps/consumer/src/main.ts
+++ apps/consumer/src/main.ts
@@ -1,16 +1,20 @@
-import { NestFactory } from '@nestjs/core'
-import { MicroserviceOptions, Transport } from '@nestjs/microservices'
-import { AppModule } from './consumer.module'
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './consumer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
-    options: {
-      host: '127.0.0.1',
-      port: 3001, // Consumer listens on port 3001
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3001, // Consumer listens on port 3001
+      },
     },
-  })
-  await app.listen()
-  console.log('Consumer microservice is listening on port 3001...')
+  );
+  await app.listen();
+  console.log('Consumer microservice is listening on port 3001...');
 }
-bootstrap()
+
+bootstrap();
--- apps/main/src/app.controller.ts
+++ apps/main/src/app.controller.ts
@@ -1,21 +1,22 @@
-import { Controller, Inject, OnModuleInit } from '@nestjs/common'
-import { ClientProxy } from '@nestjs/microservices'
+import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController implements OnModuleInit {
   constructor(@Inject('TEXT_SERVICE') private readonly client: ClientProxy) {}
 
   async onModuleInit() {
-    await this.client.connect()
-    this.sendMessage()
+    await this.client.connect();
+    this.sendMessage();
   }
 
   sendMessage() {
-    const message = 'Hello from the producer!'
+    const message = 'Hello from the producer!';
+
     this.client
       .send<string, string>('plain_text', message)
       .subscribe((response) => {
-        console.log(`Response from consumer:`, response)
-      })
+        console.log(`Response from consumer:`, response);
+      });
   }
 }
